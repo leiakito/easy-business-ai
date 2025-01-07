9e3b347b934f67186adcfbeae8fe97ab0087d9a4
Felix <2072738187yu@gamil.com>
2025-01-07 18:34:20 +0800

    feat: add seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,28 @@ import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/toaster'
 
 import type { Metadata, Viewport } from 'next'
+
 import './globals.css'
 
 const font = Space_Grotesk({ subsets: ['latin'], weight: '400' })
 
 export const metadata: Metadata = {
   title: 'LinkAI',
-  description: 'An OpenAI based chat system'
+  description: 'An OpenAI based chat system',
+  keywords: [
+    'LinkAI',
+    'business AI',
+    'AI chatbot',
+    'chatbot templates',
+    'AI-powered communication',
+    'business automation',
+    'customer service AI',
+    'AI templates',
+    'conversational AI',
+    'business intelligence',
+    'AI solutions',
+    'custom chatbots'
+  ]
 }
 
 export const viewport: Viewport = {
