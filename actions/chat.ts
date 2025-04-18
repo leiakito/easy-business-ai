59d4f5217231efc322be99976431eff2c0234c43
Felix <2072738187yu@gamil.com>
2025-04-18 13:59:36 +0800

    fix: revalid path

--- actions/chat.ts
+++ actions/chat.ts
@@ -1,7 +1,6 @@
 'use server'
 
 import { Prisma } from '@prisma/client'
-import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 import { OPEN_ROUTER_API_KEY } from '@/constant/config'
@@ -71,8 +70,6 @@ export async function updateUserTokenUsage(tokensUsed: number) {
   if (updatedSubscription.usedTokens > updatedSubscription.totalTokens) {
     console.warn(`User ${session.user.id} has exceeded their token limit`)
   }
-
-  revalidatePath('/chat', 'layout')
 
   return updatedSubscription
 }
